dbc25fca7e0aad14d62299e1d9e48f1bcd2f10a8
zaN-121 <projectdsanthos@gmail.com>
2022-09-15 18:02:52 +0700

    Modify server with hapi plugin server concept

--- src/server.js
+++ src/server.js
@@ -1,19 +1,30 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NoteValidator = require('./validator/notes');
 
-const init =  async () => {
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    const server = Hapi.server({
-        port: 5000,
-        host : process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-            cors: {
-                origin: ['*']
-            }
-        }
-    })
-    server.route(routes);
-    await server.start();
-    console.log(`server berjalan di ${server.info.uri}`)
-} 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NoteValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
 init();
