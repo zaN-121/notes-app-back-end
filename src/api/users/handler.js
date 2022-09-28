a46d3e15042361333afc24b39a10f6fdbc6a94b5
zaN-121 <projectdsanthos@gmail.com>
2022-09-28 08:30:11 +0700

    create users plugin

---
+++ src/api/users/handler.js
@@ -0,0 +1,75 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    try {
+      await this._validator.validateUserPayload(request.payload);
+  
+      const result = await this._service.addUser(request.payload);
+      const response = h.response({
+        status: 'success',
+        message: 'User berhasil ditambahkan',
+        data: {
+          userId: result,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+      const response = h.response({
+        status: 'error',
+        message: err.message,
+      });
+      response.code(500);
+      return response;
+    }
+  }
+
+  async getUserByIdHandler(request, h) {
+    try {
+      const { userId } = request.params
+      const user = await this._service.getUserById(userId);
+      const response = h.response({
+        status: 'success',
+        data: {
+          user,
+        },
+      });
+      response.code(200);
+      return response;
+    } catch (err) {
+      if (err instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: err.message,
+        });
+        response.code(err.statusCode);
+        return response;
+      }
+      const response = h.response({
+        status: 'error',
+        message: 'Terjadi kesalahan pada server kami',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
---
+++ src/api/users/routes.js
@@ -0,0 +1,13 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/users',
+    handler: handler.postUserHandler,
+  },
+  {
+    method: 'GET',
+    path: '/users/{userId}',
+    handler: handler.getUserByIdHandler,
+  },
+]
+module.exports = routes;
