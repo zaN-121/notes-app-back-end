d7b35d6f4d186da3a772df3f78a126fee38692ee
zaN-121 <projectdsanthos@gmail.com>
2022-10-02 09:57:17 +0700

    Crete verify note access function

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -7,8 +7,9 @@ const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 
 class NotesService {
-  constructor() {
+  constructor(collaborationsService) {
     this._pool = new Pool();
+    this._collaborationsService = collaborationsService;
   }
 
   async addNote({ title, body, tags, owner }) {
@@ -30,7 +31,10 @@ class NotesService {
 
   async getNotes(owner) {
     const query = {
-      text: 'SELECT * FROM notes WHERE owner = $1',
+      text: `SELECT notes.* FROM notes
+      LEFT JOIN collaborations ON collaborations.note_id = notes.id
+      WHERE notes.owner = $1 OR collaborations.user_id = $1
+      GROUP BY notes.id`,
       values: [owner],
     };
     const result = await this._pool.query(query);
@@ -39,7 +43,9 @@ class NotesService {
 
   async getNoteById(id) {
     const query = {
-      text: "SELECT * FROM notes WHERE id = $1",
+      text: `SELECT notes.*, users.username 
+      FROM notes LEFT JOIN users ON users.id = notes.owner
+      WHERE notes.id = $1`,
       values: [id],
     }
     const result = await this._pool.query(query);
@@ -89,6 +95,22 @@ class NotesService {
     
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    // priksa dulu apakah pengakses note adalah ownernya 
+    try {
+      await this.verifyNoteOwner(noteId, userId)
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationsService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
