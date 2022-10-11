fe5a58106ae77df0e4d8c86f200e1fa71f8a438a
zaN-121 <projectdsanthos@gmail.com>
2022-10-11 09:56:20 +0700

    Create uploads api plugin

---
+++ src/api/uploads/index.js
@@ -0,0 +1,17 @@
+const UploadsHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'uploads',
+  version: '1.0.0',
+  register: async (
+    server,
+    {
+      service,
+      validator,
+    },
+  ) => {
+    const uploadsHandler = new UploadsHandler(service, validator);
+    server.route(routes(uploadsHandler));
+  }
+}
